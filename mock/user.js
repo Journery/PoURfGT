7807f3163b354755593cddbf9c3c19ef2491d83c
Journery <383440764@qq.com>
2021-05-16 19:06:34 +0800

    Initial commit

---
+++ mock/user.js
@@ -0,0 +1,84 @@
+
+const tokens = {
+  teacher: {
+    token: 'teacher-token'
+  },
+  student: {
+    token: 'student-token'
+  }
+}
+
+const users = {
+  'teacher-token': {
+    roles: ['teacher'],
+    introduction: 'I am a teacher',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Teacher'
+  },
+  'student-token': {
+    roles: ['student'],
+    introduction: 'I am an student',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Student'
+  }
+}
+
+module.exports = [
+  // user login
+  {
+    url: '/vue-admin-template/user/login',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (!token) {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  },
+
+  // get user info
+  {
+    url: '/vue-admin-template/user/info\.*',
+    type: 'get',
+    response: config => {
+      const { token } = config.query
+      const info = users[token]
+
+      // mock error
+      if (!info) {
+        return {
+          code: 50008,
+          message: 'Login failed, unable to get user details.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: info
+      }
+    }
+  },
+
+  // user logout
+  {
+    url: '/vue-admin-template/user/logout',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  }
+]
---
+++ src/views/group/GroupTable.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="app-container">
+    <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" v-bind:height="tableH" border fit
+      highlight-current-row>
+      <el-table-column align="center" label="学号" width="150">
+        <template slot-scope="scope">
+          {{ scope.$index }}
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width="150" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="课程" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.pageviews }}
+        </template>
+      </el-table-column>
+      <el-table-column label="小组名称" width="200" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.status }}
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="小组编号" width="110">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.display_time }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="checkPermission(['teacher'])" align="center" label="操作" width="110" fixed="right">
+        <template slot-scope="scope">
+          <el-button
+          @click.native.prevent="deleteRow(scope.$index, list)"
+          type="danger"
+          size="mini">
+            <i class="el-icon-delete-solid" />
+            删除
+          </el-button>
+        </template>
+
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  import {
+    getList
+  } from '@/api/table'
+  import permission from '@/directive/permission/index.js' // 权限判断指令
+  import checkPermission from '@/utils/permission'
+  export default {
+    name: 'GroupTable',
+    props: ['tableH'],
+    directives:{ permission },
+    data() {
+      return {
+        list: null,
+        listLoading: true,
+      }
+    },
+    filters: {
+      statusFilter(status) {
+        const statusMap = {
+          published: 'success',
+          draft: 'gray',
+          deleted: 'danger'
+        }
+        return statusMap[status]
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      checkPermission,
+      fetchData() {
+        this.listLoading = true
+        getList().then(response => {
+          this.list = response.data.items
+          this.listLoading = false
+        })
+      },
+      deleteRow(index, rows) {
+        rows.splice(index, 1);
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
